57e36c02e68943ec1c854ab4ae1345a20ae3d5b6
ASUS <ASUS@LAPTOP-D8LBK4KJ>
2021-07-23 03:36:28 +0700

    update UI

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Button from '@atlaskit/button'
+import Todo from './Todo'
 
 export default function TodoList() {
     return (
         <div>
-            
+            <Todo />
+            <Todo />
+            <Todo />
         </div>
     )
 }
