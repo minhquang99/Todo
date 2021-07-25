bf7be95faeefdc45be6c04bd115ee7ef96322715
ASUS <ASUS@LAPTOP-D8LBK4KJ>
2021-07-26 00:38:14 +0700

    hooks

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import Button from "@atlaskit/button"
 import { useState } from "react";
 import { v4 } from 'uuid';
 
+const TODO_APP_STORAGE = 'TODO_APP';
+
 function App() {
   //  state: dữ liệu 'nội tại' của component hiện tại
   // prop: các dữ liệu truyền từ element cha
   const [todoList, setTodoList] = useState([]);
   const [textInput, setInputText] = useState("");
+
+  useEffect(() => {
+    const storageTodoList = localStorage.getItem(TODO_APP_STORAGE);
+    if(localStorage.getItem(storageTodoList)){
+      setTodoList(JSON.parse(storageTodoList));
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE, JSON.stringify(todoList));
+  }, [todoList]);
 
   const onTextInputChange = 
     (e) => {
@@ -17,11 +30,14 @@ function App() {
 
   const onAddButtonClick = 
     (e) => {
-      setTodoList([{...todoList, id: v4(), name: textInput, isCompleted: false}]);
+      setTodoList([{id: v4(), name: textInput, isCompleted: false}, ...todoList]);
+      setInputText("");
     }
 
+  const onCheckBtnClick = (id) => {
+    setTodoList(prevSate => prevSate.map(todo => todo.id === id ? {...todo, isCompleted: true} : todo));
+  }
 
-  
   return (
     <div>
       <h3>Danh sách cần làm</h3>
@@ -33,7 +49,7 @@ function App() {
       value={textInput}
       onChange={onTextInputChange}
       ></Textfield>
-      <TodoList todoList={todoList}/> 
+      <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick}/> 
     </div>
   );
 }
